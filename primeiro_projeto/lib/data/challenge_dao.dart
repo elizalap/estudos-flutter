8f364e9ca7862f9beaf775f9ec5a4e27019116bb
Eliza Lima <elizapaoliello@outlook.com>
2024-02-22 00:23:50 -0300

    Implementando função find()

--- primeiro_projeto/lib/data/challenge_dao.dart
+++ primeiro_projeto/lib/data/challenge_dao.dart
@@ -2,8 +2,7 @@ import 'package:alura_flutter_curso_1/components/challenge.dart';
 import 'package:alura_flutter_curso_1/data/database.dart';
 import 'package:sqflite/sqflite.dart';
 
-class ChallengeDao
-{
+class ChallengeDao {
   static const String tableSql = 'CREATE TABLE $_tableName('
       '$_name TEXT, '
       '$_difficulty INTEGER,'
@@ -14,23 +13,34 @@ class ChallengeDao
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  save(Challenge desafio) async{}
-  
-  Future<List<Challenge>> findAll() async{
+  save(Challenge desafio) async {}
+
+  Future<List<Challenge>> findAll() async {
     final Database bancoDeDados = await getDatabase();
-    final List<Map<String, dynamic>> result = await bancoDeDados.query('challenge.db');
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tableName);
     return toList(result);
   }
 
-  List<Challenge> toList(List<Map<String,dynamic>> mapaDeDesafios){
+  List<Challenge> toList(List<Map<String, dynamic>> mapaDeDesafios) {
     final List<Challenge> desafios = [];
-    for(Map<String,dynamic> linha in mapaDeDesafios){
-      final Challenge desafio = Challenge(linha[_name], linha[_image] , linha[_difficulty]);
+    for (Map<String, dynamic> linha in mapaDeDesafios) {
+      final Challenge desafio =
+          Challenge(linha[_name], linha[_image], linha[_difficulty]);
       desafios.add(desafio);
     }
     return desafios;
   }
-  Future<List<Challenge>> find(String nome) async{}
-  delete(String nome) async{}
-}
 
+  Future<List<Challenge>> find(String nome) async {
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(
+      _tableName,
+      where: '$_name = ?',
+      whereArgs: [nome],
+    );
+    return toList(result);
+  }
+
+  delete(String nome) async {}
+}
