ff61dfd3682e97c998c7ca4bb47945822233f9ee
Eliza Lima <elizapaoliello@outlook.com>
2024-05-08 10:06:31 -0300

    Adicionando interceptadores

--- flutter_webapi_first_course-main/lib/services/journal_service.dart
+++ flutter_webapi_first_course-main/lib/services/journal_service.dart
@@ -1,8 +1,12 @@
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/intercepted_client.dart';
 
 class JournalService {
   static const String url = "http://10.10.0.101:3000/";
   static const String resource = "learnhttp/";
+
+  http.Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
   String getURL() {
     return "$url$resource";
@@ -10,11 +14,11 @@ class JournalService {
 
   //TODO: Substituir getURL por getURI
   void register(String content) {
-    http.post(Uri.parse(getURL()), body: {'content': content});
+    client.post(Uri.parse(getURL()), body: {'content': content});
   }
 
   void get() async {
-    http.Response response = await http.get(Uri.parse(getURL()));
+    http.Response response = await client.get(Uri.parse(getURL()));
     print(response.body);
   }
 }
