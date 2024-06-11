e5efdc32bec8b4951f2b3ed37433a36e62e90fe3
Eliza Lima <elizapaoliello@outlook.com>
2024-06-11 18:16:13 -0300

    Adicionando rota nomeada

--- flutter_webapi_first_course-main/lib/main.dart
+++ flutter_webapi_first_course-main/lib/main.dart
@@ -28,16 +28,24 @@ class MyApp extends StatelessWidget {
           titleTextStyle: TextStyle(
             color: Colors.white,
           ),
-          actionsIconTheme: IconThemeData(color: Colors.white)
+          actionsIconTheme: IconThemeData(color: Colors.white),
+            iconTheme: IconThemeData(color: Colors.white),
         ),
         //textTheme: GoogleFonts.bitterTextTheme(),
       ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
-      initialRoute: "add-journal",
+      initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
-        "add-journal": (context)=> AddJournalScreen(journal: Journal(id: "id", content: "content", createdAt: DateTime.now(), updatedAt: DateTime.now())),
+      },
+      onGenerateRoute: (settings){
+        if(settings.name == 'add-journal'){
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(builder: (context){
+            return AddJournalScreen(journal: journal);
+          });
+        }
       },
     );
   }
